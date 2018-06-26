f516435b41224331d2a6ab4c5b733ef192e161ae
Yuta Oosone <oosoney@gkt.co.jp>
2018-06-26 18:15:42 +0900

    LandInfoと同様に全要素に対応

---
+++ scraping_source/dbconnection.py
@@ -0,0 +1,53 @@
+import psycopg2
+
+class DbConnect:
+    def __init__(self):
+        self.connection = psycopg2.connect(
+            "host=192.168.0.109 port=5432 dbname=postgres user=postgres password=scrapingland"
+        )
+        self.cursor = self.connection.cursor()
+
+    def db_select_all(self, table):
+        sql = "SELECT * FROM " + table
+        self.cursor.execute(sql)
+        res = self.cursor.fetchall()
+        print(res)
+
+    def db_insert_test(self, table, columns, info_tuple):
+        sql = "INSERT INTO " + table + " (" + columns + ") VALUES(%s, %s, %s, %s)"
+        self.cursor.execute(sql, info_tuple)
+
+    def db_insert1(self, table, columns, info_tuple):
+        sql = "INSERT INTO " + table + " (" + columns + ") VALUES("\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            ")"
+        self.cursor.execute(sql, info_tuple)
+
+    def db_insert2(self, table, columns, info_tuple):
+        sql = "INSERT INTO " + table + "(" + columns + ") VALUES("\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s, %s, %s, %s"\
+            "%s, %s"\
+            ")"
+        self.cursor.execute(sql, info_tuple)
+
+    def db_rollback(self):
+        print("rollback")
+        self.connection.rollback()
+
+    def db_commit(self):
+        print("commit")
+        self.connection.commit()
+
+    def db_close(self):
+        self.connection.close()
