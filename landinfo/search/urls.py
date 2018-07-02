2317fea12b8036464caa4040f51ba81da0e72dcc
sankouenzansi <sankouenzansi@gmail.com>
2018-07-02 18:37:19 +0900

    viewsのソースをメソッド化、及び必要ない部分の削除

--- landinfo/search/urls.py
+++ landinfo/search/urls.py
@@ -4,16 +4,8 @@ from . import views
 
 app_name = 'search'
 urlpatterns = [
-    # ex: /search/
-    path('', views.index, name='index'),
-
-    path('<int:landinfo_id>/', views.detail, name='detail'),
-
-    #path('<int:landinfo_id>/one/', views.one, name='one'),
     path('<int:landinfo_id>/one/', views.one, name='one'),
 
     path('all/', views.all, name='all'),
-
-#    path('<int:landinfo>/retrieval', views),
 ]
 
