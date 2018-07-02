de1bde46b123feedced176a72db74e99092c28bb
sgyuuki <failchaossy28ffsm@gmail.com>
2018-07-02 18:51:20 +0900

    form関連です

--- landinfo/search/forms.py
+++ landinfo/search/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
+from . import models
 class SearchForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=200)
+    title = forms.CharField( label='タイトル', max_length=200)
