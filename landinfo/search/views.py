7f917b0c8909b2082128e400f00b76898d2e3998
sankouenzansi <sankouenzansi@gmail.com>
2018-07-02 16:34:22 +0900

    全件表示からリンクで詳細へ飛べる機能を実装 cssファイルを配置

--- landinfo/search/views.py
+++ landinfo/search/views.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 from sqlalchemy.sql import select
 
+#from .forms import SearchForm
 # Create your views here.
 def index(request):
 #    latest_landinfo_list = LandInfo[:5]
@@ -25,7 +26,6 @@ def detail(request, landinfo_id):
     except LandInfo.DoesNotExist:
         raise Http404("土地が見つかりません")
     return render(request, 'search/detail.html', {'landinfo': landinfo})
-
 
 
 def one(request, landinfo_id):
@@ -148,9 +148,9 @@ def all(request):
         Column('tsubo_unit_price', String),
         Column('units_sold_total_units', String),
     )
-    s = select([land_info])
+    s = select([land_info.c.id,land_info.c.title])
     result = conn.execute(s) 
-    data = result.fetchall()[5:10]
+    data = result.fetchall()
     return render(request, 'search/all.html',{'app':'全て','columns':data})
 
 def retrieval(request, landinfo_id):
@@ -164,3 +164,18 @@ def retrieval(request, landinfo_id):
         })
     else:
         return HttpResponseRedirect(reverse('search:results', args=(landinfo.id,)))
+
+def get_title(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+
+    else:
+        form = SearchForm()
+
+    return render(request, 'results.html', {'form': form})
+        
