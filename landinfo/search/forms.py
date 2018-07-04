ae2ec9ba7a1598a5a07d4a87e26adb0bd2ffcb4d
sankouenzansi <sankouenzansi@gmail.com>
2018-07-04 16:37:13 +0900

    select機能:条件をタイトル、所在地で検索する機能を実装 その際、select機能をクラス化、機能名変更、合わせて、viewsも変更

--- landinfo/search/forms.py
+++ landinfo/search/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 from . import models
 class SearchForm(forms.Form):
-    title = forms.CharField( label='タイトル', max_length=200)
+    title = forms.CharField(required=False,label='タイトル', max_length=200)
+    location = forms.CharField( required=False,label='所在地', max_length=200)
+class SearchFormLocate(forms.Form):
+    location = forms.CharField( label='所在地', max_length=200)
--- landinfo/search/views.py
+++ landinfo/search/views.py
@@ -2,29 +2,33 @@ from django.http import Http404, HttpResponse,HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
-from scraping_source.land_info_scraping import land_info_scraping
+
 from search.DBconnection import selectland
 #from .forms import SearchForm
-from .forms import SearchForm
+from .forms import SearchForm,SearchFormLocate
+
+sland = selectland()
 
 def one(request, landinfo_id):
-    data = selectland('one',landinfo_id,0,0)
+    data = sland.selectone(landinfo_id)
     return render(request, 'search/one.html',{'app':'ひとつだけ','columns':data})
 
 def all(request):
-    data = selectland('all',0,0,0)
+    data = sland.selectall()
     return render(request, 'search/all.html',{'columns':data})
 
 def searchforms(request):
     if request.method == "POST":
+        formlocate = SearchFormLocate(data=request.POST)
         form = SearchForm(data=request.POST)
-        if form.is_valid():
+        if form.is_valid() or formlocate.is_valid:
             #このif文の中に処理を書く
-            data = selectland('search',0,0,request.POST['title'])
+            data = sland.selectsearch(request.POST)
             return render(request, 'search/search.html', {'form': form,'columns':data})
     else:
         form = SearchForm()
     return render(request, 'search/search.html', {'form': form})
+
 
 def get_title(request):
     # if this is a POST request we need to process the form data
