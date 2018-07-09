863eaa3429ac34b1b29b0442385ab418ac4c2a2e
sankouenzansi <sankouenzansi@gmail.com>
2018-07-09 10:52:22 +0900

    全件表示サイトのレイアウト変更 Noneを「なし」に変換する処理を実装

--- landinfo/search/views.py
+++ landinfo/search/views.py
@@ -2,16 +2,18 @@ from django.http import Http404, HttpResponse,HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
-
 from search.DBconnection import selectland
-#from .forms import SearchForm
+from .LandInfotitle import landcolumns
 from .forms import SearchForm,SearchFormLocate
 
 sland = selectland()
+names = landcolumns()
 
 def one(request, landinfo_id):
     data = sland.selectone(landinfo_id)
-    return render(request, 'search/one.html',{'app':'ひとつだけ','columns':data})
+    name = names.landname
+    return render(request, 'search/one.html',{'app':'ひとつだけ','columns':data,'name':name})
+    #return render(request, 'search/one.html',{'app':'ひとつだけ','columns':data,})
 
 def all(request):
     data = sland.selectall()
@@ -21,7 +23,7 @@ def searchforms(request):
     if request.method == "POST":
         formlocate = SearchFormLocate(data=request.POST)
         form = SearchForm(data=request.POST)
-        if form.is_valid() or formlocate.is_valid:
+        if form.is_valid() :
             #このif文の中に処理を書く
             data = sland.selectsearch(request.POST)
             return render(request, 'search/search.html', {'form': form,'columns':data})
